a720f694b567c9a40a6aaddaf46f5f82a9036c97
indika <indika@misynergy.com>
2026-01-28 10:53:22 +0530

    Initial commit

---
+++ src/main/java/com/example/neo4jintegration/Neo4jIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo4jintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neo4jintegration/Neo4jIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neo4jintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
